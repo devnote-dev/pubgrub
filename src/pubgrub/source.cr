49b9250a680b36c77d19c134766cc5a9489e2b44
devnote-dev <devnote.dev75@gmail.com>
2023-04-22 01:26:03 +0100

    feat: implement abstracts for source

--- src/pubgrub/source.cr
+++ src/pubgrub/source.cr
@@ -2,6 +2,18 @@ module PubGrub
   abstract class PackageSource
     abstract def root : Hash(String, String)
     abstract def packages : Hash(String, Hash(String, Hash(String, String)))
+    abstract def all_versions_for(package : Package) : Array(String)
+    abstract def dependencies_for(package : Package, version : String) : Hash(String, String)
+    abstract def parse_dependency(package : Package, dependency) : Version
+
+    def root_dependencies : Hash(Package, String)
+      dependencies_for(BasicPackage.new("root", 0), 0)
+    end
+
+    def sort_versions_by_preferred(package : Package, sorted_versions : Array(Version)) : Array(Version)
+      indexes = version_indexes[package]
+      sorted_versions.sort_by { |v| indexes[v] }
+    end
 
     def root(dependencies : Hash(String, String)) : Nil
       @root.merge! dependencies
@@ -34,6 +46,18 @@ module PubGrub
       @packages = Hash(String, Hash(String, Hash(String, String))).new
 
       yield self
+    end
+
+    def all_versions_for(package : Package) : Array(String)
+      @packages[package.name].keys
+    end
+
+    def dependencies_for(package : Package, version : String) : Hash(String, String)
+      @packages[package.name][version]
+    end
+
+    def parse_dependency(package : Package, dependency) : Version
+      Version::Constraint.new SemanticVersion.parse(dependency)
     end
   end
 end
