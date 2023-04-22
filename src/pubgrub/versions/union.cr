07816115949a2777f2a9f0cda6e4504b507f0866
devnote-dev <devnote.dev75@gmail.com>
2023-04-23 00:18:20 +0100

    feat(versions): implement more of union

--- src/pubgrub/versions/union.cr
+++ src/pubgrub/versions/union.cr
@@ -83,6 +83,60 @@ module PubGrub
       end
 
       def difference(other : Version::Constraint) : Version::Constraint
+        # TODO
+        other
+      end
+
+      private def ranges_for(constraint : Version::Constraint) : Array(Version::Range)
+        return [] of Version::Range if constraint.empty?
+
+        case constraint
+        when Version::Range then constraint.ranges
+        when Version::Union then [constraint]
+        else raise ArgumentError.new "unknown version constraint type: #{typeof(constraint)}"
+        end
+      end
+
+      private def adjacent?(first : Version::Range, second : Version::Range) : Bool
+        return false if first.max != second.min
+
+        (first.include_max? && !second.include_min?) || (!first.include_max? && second.include_min?)
+      end
+
+      private def allows_higher?(first : Version::Range, second : Version::Range) : Bool
+        return !second.max.nil? if first.max.nil?
+        return false if second.max.nil?
+
+        case first.max.not_nil! <=> second.max.not_nil!
+        when 1 then true
+        when -1 then false
+        else first.include_max? && !second.include_max?
+        end
+      end
+
+      private def allows_lower?(first : Version::Range, second : Version::Range) : Bool
+        return !second.min.nil? if first.min.nil?
+        return false if second.min.nil?
+
+        case first.min.not_nil! <=> second.min.not_nil!
+        when 1 then false
+        when -1 then true
+        else first.include_min? && !second.include_min?
+        end
+      end
+
+      private def strictly_higher?(first : Version::Range, second : Version::Range) : Bool
+        strictly_lower?(second, first)
+      end
+
+      private def strictly_lower?(first : Version::Range, second : Version::Range) : Bool
+        return false if first.max.nil? || second.max.nil?
+
+        case first.max.not_nil! <=> second.min.not_nil!
+        when 1 then false
+        when -1 then true
+        else !first.include_max? || !second.include_min?
+        end
       end
     end
   end
