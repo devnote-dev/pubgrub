48f12b56e4a78ed7922858e62e02d855b20211ac
devnote-dev <devnote.dev75@gmail.com>
2023-04-23 23:06:30 +0100

    refactor: reimplement assignment from pub

--- src/pubgrub/assignment.cr
+++ src/pubgrub/assignment.cr
@@ -1,16 +1,23 @@
 module PubGrub
-  class Assignment
-    getter term : Term
-    getter cause : Cause
-    getter decision_level
-    getter index
+  class Assignment < Term
+    getter decision_level : Int32
+    getter index : Int32
+    getter cause : Incompatibility?
 
-    def initialize(@term : Term, @cause : Cause, @decision_level, @index)
+    def initialize(package : Package, positive : Bool, @cause : Cause?, @decision_level : Int32, @index : Int32)
+      super package, positive
     end
 
-    def self.decision(package : Package, version : Int32, decision_level, index)
-      term = Term.new(Version::Constraint.exact(package, version), true)
-      new(term, Cause::Decision.new, decision_level, index)
+    def self.decision(package : Package, decision_level : Int32, index : Int32)
+      new package, true, nil, decision_level, index
+    end
+
+    def self.derivation(package : Package, positive : Bool, cause : Cause?, decision_level : Int32, index : Int32)
+      new package, positive, cause, decision_level, index
+    end
+
+    def decision? : Bool
+      @cause.nil?
     end
   end
 end
